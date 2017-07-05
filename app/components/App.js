ac49b790adaa7b9b6493e84ad6837c0906bac153
Nicholai de Guzman <nic@imac>
2017-07-05 14:04:35 -0500

    added react router
    npm install --save react-router-dom

--- app/components/App.js
+++ app/components/App.js
@@ -1,4 +1,9 @@
 var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Home = require('./Home');
 
 class App extends React.Component {
   render() {
---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className='home-container'>
+        <h1>Home component</h1>
+
+      </div>
+    )
+  }
+}
+
+module.exports = Home;
